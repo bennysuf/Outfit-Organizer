42ccde233a703939c67dd6d25ada2a02ab5d2b87
Benny Sufrin <bennysuf@gmail.com>
2022-11-30 15:28:56 -0500

    adds dropdown for days of the week, button to add date

--- my-app/src/CardsContainer.js
+++ my-app/src/CardsContainer.js
@@ -1,6 +1,18 @@
 import Cards from "./Cards"
+import {useState} from "react"
 
 function CardsContainer({ clothes }) {
+ const [date, setDate] = useState("")
+
+    function handleDateChange(e) {
+        setDate(e.target.value)
+    }
+
+    function handleDateClick(){
+        if (date !== "Add to wardrobe"){
+            console.log(date)
+        }
+    }
 
     const clotheCards = clothes.map((item) => {
         const { id, name, description, image } = item
@@ -9,6 +21,17 @@ function CardsContainer({ clothes }) {
                 <h1>{name}</h1>
                 <p>{description}</p>
                 <img src={image} alt="" />
+                <select onChange={handleDateChange}>
+                    <option>Add to wardrobe</option>
+                    <option>Sunday</option>
+                    <option>Monday</option>
+                    <option>Tuesday</option>
+                    <option>Wednesday</option>
+                    <option>Thursday</option>
+                    <option>Friday</option>
+                    <option>Saturday</option>
+                </select>
+                <button onClick={handleDateClick}>Add</button>
             </li>
         )
     })
