1ec159da36db26b977b942904688b0a969b8328f
Benny Sufrin <bennysuf@gmail.com>
2022-12-02 11:58:32 -0500

    creates div with label and button

---
+++ my-app/src/BotCheck.js
@@ -0,0 +1,13 @@
+
+function BotCheck(){
+    return (
+        <div className="robot">
+        <label>Confirm you aren't a robot</label>
+        <br />
+        <button>click</button>
+        </div >
+
+    )
+}
+
+export default BotCheck;
