96b26b42898491715143d1b9919d03bc87385dd6
bennysuf <109253075+bennysuf@users.noreply.github.com>
2022-12-05 19:20:30 -0500

    Update BotCheck.js

--- my-app/src/BotCheck.js
+++ my-app/src/BotCheck.js
@@ -6,9 +6,7 @@ function BotCheck() {
         <div className="robot">
             <NavLink to="/home"><input type="checkbox" checked={null} style={{ height: "30px", width: "75px" }}></input></NavLink>
             <label className="lable">I'm not a robot</label>
-            
         </div >
-
     )
 }
 
