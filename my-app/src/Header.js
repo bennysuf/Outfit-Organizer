a07f02dbbb1ea643800ca66da8215842414040b6
Benny Sufrin <bennysuf@gmail.com>
2022-12-07 13:13:11 -0500

    adds navlink to home/new

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,10 +1,10 @@
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 function Header() {
     return (
         <>
-    <h1 className="header">Outfit Organizer</h1>
-    <NavLink to="/home/new" className="add"><button>Add new item</button></NavLink>
+            <h1 className="header">Outfit Organizer</h1>
+            <NavLink to="/home/new" className="add"><button>Add new item</button></NavLink>
         </>
     )
 }
